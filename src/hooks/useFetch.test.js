55b53bc6c08920cc08da822c2e89072c897eaf40
EArcigaV <eduardo.arcigav@gmail.com>
2022-04-27 14:21:00 -0500

    added a use case to useFetch.test.js

--- src/hooks/useFetch.test.js
+++ src/hooks/useFetch.test.js
@@ -1,1 +1,13 @@
+import { screen, render } from "@testing-library/react";
+
+import Content from "../components/Content/Content";
+
+test("it should have the correct  item Jason Dain", async () => {
+  render(<Content />);
+  const findItem = await screen.findByRole("heading", {
+    name: "by: Jason Dain",
+  });
+  expect(findItem).toBeVisible();
+});
+
 test.todo("should fetch correctly");
